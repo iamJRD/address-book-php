26eafd76e9257d084fd5a71bbc32edb963eae0a4
Jared Beckler <jared.beckler@gmail.com>
2016-02-12 08:46:12 -0800

    add twig template and functionality for viewing added contact

--- app/app.php
+++ app/app.php
@@ -16,5 +16,11 @@
         return $app['twig']->render('home.html.twig', array('contacts' => Contact::getAll()));
     });
 
+    $app->post('/added_contact', function() use ($app) {
+        $contact = new Contact($_POST['contact_name'], $_POST['contact_phone_number'], $_POST['contact_address']);
+        $contact->save();
+        return $app['twig']->render('added_contact.html.twig', array('newcontact' => $contact));
+    });
+
     return $app;
 ?>
