b7381b4ebb7beb982e9e29970823b4d437c4ea89
Jared Beckler <jared.beckler@gmail.com>
2016-02-12 08:26:28 -0800

    add twig template for homepage/form for adding contacts

--- app/app.php
+++ app/app.php
@@ -2,7 +2,19 @@
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/Contact.php';
 
+    session_start();
+    if(empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
     $app = new Silex\Application();
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('home.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    return $app;
 ?>
